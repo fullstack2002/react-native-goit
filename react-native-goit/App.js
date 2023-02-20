fdf77e30b933c6e0048bdf299e3e83e9dca7d21b
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-02-20 22:47:35 +0200

    bugs

--- react-native-goit/App.js
+++ react-native-goit/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const MainStack = createStackNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MainStack.Navigator initialRouteName="Home">
+       <MainStack.Screen name="Registration" component={RegistrationScreen} />
+          <MainStack.Screen name="Login" component={LoginScreen}
+            options={{ title: 'Enter the application' }}
+        />
+        </MainStack.Navigator>
     </View>
   );
 }
